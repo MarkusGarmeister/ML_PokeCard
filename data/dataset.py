9580e411764912b2fdb16dc173b6e946f1d311d6
MarkusGarmeister <markusgarmeister@gmail.com>
2026-04-20 20:19:06 +0200

    add img download

--- data/dataset.py
+++ data/dataset.py
@@ -1,16 +1,45 @@
 from datasets import load_dataset
 import pandas as pd
+import requests
 
 
 class Dataset:
     def __init__(self):
         self.DATASET_PATH = "data/dataset/"
+        self.IMAGE_PATH = "data/dataset/img/"
 
     def dowload_dataset(self) -> pd.DataFrame:
         ds = load_dataset("tooni/pokemoncards", data_files="cards.csv", split="train")
-        ds.to_csv(self.DATASET_PATH + "cards.csv", index=False)
+        df = pd.DataFrame(ds)
+        return df
+
+    def get_dataset(self) -> pd.DataFrame:
+        if self.DATASET_PATH + "cards.csv" is None:
+            df = self.dowload_dataset()
+            return df
+        else:
+            df = pd.read_csv(self.DATASET_PATH + "cards.csv")
+            return df
+
+    def download_images(self, df: pd.DataFrame):
+        for index, row in df.iterrows():
+            image_url = row["small_image_source"]
+            image_url = image_url.strip('"')
+            print(image_url)
+            image_name = f"{row['id']}.png"
+            image_path = self.IMAGE_PATH + image_name
+            try:
+                response = requests.get(image_url)
+                response.raise_for_status()
+                with open(image_path, "wb") as f:
+                    f.write(response.content)
+                print(f"Downloaded {image_name}")
+
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to download {image_name}: {e}")
 
 
 if __name__ == "__main__":
     dataset = Dataset()
     df = dataset.dowload_dataset()
+    dataset.download_images(df)
